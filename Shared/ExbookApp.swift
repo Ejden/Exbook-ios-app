acc909d4c35c1398d7db4a3f66e54d9faf3f0a48
Adrian Stypinski <adrian.stypinski@allegro.pl>
2022-07-18 19:19:55 +0200

    Initial Commit

---
+++ Shared/ExbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExbookApp.swift
+//  Shared
+//
+//  Created by Adrian Stypinski on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
